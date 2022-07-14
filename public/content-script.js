168f52fd34c349b6e5f7148070d89b74f6506f53
Nick Jiang <nickj@berkeley.edu>
2022-07-14 09:59:04 -0700

    containing block script written

--- public/content-script.js
+++ public/content-script.js
@@ -79,3 +79,68 @@ function listenForAttributeChanges(observer, element) {
     observer.disconnect();
     observer.observe(element, { attributes : true, attributeFilter : ['style', 'class'] }); 
 }
+
+function getContainingBlock(element) {
+    // taken from popper.js
+    function getStyleComputedProperty(element, property) {
+        if (element.nodeType !== 1) {
+        return [];
+        }
+        // NOTE: 1 DOM access here
+        const window = element.ownerDocument.defaultView;
+        const css = window.getComputedStyle(element, null);
+        return property ? css[property] : css;
+    }
+
+    // Relevant for static, relative, and sticky positioning
+    function testFormattingContext(element) {
+        let displayVal = getStyleComputedProperty(element, 'display')
+        if (['block', 'inline-block', 'flex', 'grid'].indexOf(displayVal) !== -1) {
+            return true;
+        }
+        return false;
+    }
+
+    // Relevant for ONLY absolute
+    function testAbsolute(element) {
+        let position = getStyleComputedProperty(element, 'position')
+        if (position !== 'static') {
+            return true;
+        } else {
+            return testAbsoluteFixed(element);
+        }
+    }
+
+    // Relevant for BOTH absolute and fixed
+    function testAbsoluteFixed(node) {
+        let test; let cs = getComputedStyle(node);
+        test = cs.getPropertyValue('transform');   if (test !== 'none')  { return true; }
+        test = cs.getPropertyValue('perspective'); if (test !== 'none')  { return true; }
+        test = cs.getPropertyValue('filter');      if (test !== 'none')  { return true; }
+        test = cs.getPropertyValue('contain');     if (test === 'paint') { return true; }
+        test = cs.getPropertyValue('will-change'); if ([
+            'transform', 'perspective', 'filter'
+        ].includes(test)) { return true; }
+        test = cs.getPropertyValue('backdrop-filter'); if (test !== 'none') { return true; }
+        return false;
+    }
+
+    let position = getStyleComputedProperty(element, 'position');
+    while ((element = element.parentElement)) {
+        if (['static', 'relative', 'sticky'].includes(position) && testFormattingContext(element)) {
+            break;
+        } else if (position === 'absolute' && testAbsolute(element)) {
+            break;
+        } else if (position === 'fixed' && testAbsoluteFixed(element)) {
+            break;
+        }
+    }
+    if (element) {
+        return {
+            'containingBlock': element,
+            'styles': getComputedStyle(element)
+        }
+    } else {
+        return null;
+    }
+}
